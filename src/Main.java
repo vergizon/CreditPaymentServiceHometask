2bf2daea46c18f6473a0e8320eb1bb2bdb346b51
Alex Vergizov <alvergizoff@gmail.com>
2022-07-27 07:08:36 +0300

    Main corrected

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
         double monthPayment1year = service.calculate(9.99, 1000000, 1);
-        int res = (int) monthPayment1year;
+        int result = (int) monthPayment1year;
         System.out.println();
         System.out.println("ставка 9.99%; сумма кредита 1000000 рублей; срок 1 год");
-        System.out.println("ежемесячный платеж составляет: " + res);
+        System.out.println("ежемесячный платеж составляет: " + result);
 
         double monthPayment2years = service.calculate(9.99, 1000000, 2);
-        res = (int) monthPayment2years;
+        result = (int) monthPayment2years;
         System.out.println();
         System.out.println("ставка 9.99%; сумма кредита 1000000 рублей; срок 2 года");
-        System.out.println("ежемесячный платеж составляет: " + res);
+        System.out.println("ежемесячный платеж составляет: " + result);
 
         double monthPayment3years = service.calculate(9.99, 1000000, 3);
-        res = (int) monthPayment3years;
+        result = (int) monthPayment3years;
         System.out.println();
         System.out.println("ставка 9.99%; сумма кредита 1000000 рублей; срок 3 года");
-        System.out.println("ежемесячный платеж составляет: " + res);
+        System.out.println("ежемесячный платеж составляет: " + result);
     }
 }
